16318208da7c6bba68ebd7d96d007225e18c7843
Clayton LOPES <clopes@c4r18s5.wethinkcode.co.za>
2019-06-13 08:18:04 +0200

    strsplit update

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,27 +6,13 @@
 /*   By: clopes <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/10 09:29:00 by clopes            #+#    #+#             */
-/*   Updated: 2019/06/10 16:29:09 by clopes           ###   ########.fr       */
+/*   Updated: 2019/06/12 16:40:05 by clopes           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
+#include <stdio.h>
 // count words in str delimited by c
-static int	ft_wordcount(char *s, char c)
-{
-	int k;
-	int i;
-
-	while(s[k])
-	{
-		if (s[k] == c && s[k + 1] != c)
-			i++;
-		k++;
-	}
-	return (i);
-}
-// count length of a string upto c
 static int	ft_wordlen(char *s, char c)
 {
 	int k;
@@ -34,27 +20,52 @@ static int	ft_wordlen(char *s, char c)
 
 	i = 0;
 	k = 0;
-	while(s[k])
+	while (s[k] && s[k] != c)
+	{
+		k++;
+		i++;
+	}
+	return (i);
+}
+static int	ft_wordcount(char *s, char c)
+{
+	int k;
+	int wordcount;
+	int i;
+
+	wordcount = 0;
+	i = 0;
+	k = 0;
+	while (s[k])
 	{
 		if (s[k] == c)
-			k++;
-		else 
 		{
-			i++;
 			k++;
+			wordcount = 0;
 		}
+		if (s[k] != c && wordcount == 0)
+		{
+			wordcount = 1;
+			k++;
+			i++;
+		}
+		k++;
 	}
 	return (i);
 }
 //maloc for word count and strlen
 char	**ft_strsplit(char const *s, char c)
 {
-	char	*arr[ft_wordlen];
-	int		len;
+	char	**arr;
+	int		x;
 	int		k;
+	int		i;
 
-	len = ft_strlen(s);
-	if (!(arr = (char *)malloc(sizeof (char * len))))
+	k = 0;
+	x = 0;
+	if (s == NULL)
+		return (NULL);
+	if (!(arr = malloc(sizeof(arr) * (ft_wordcount((char *)s, c) + 1))))
 		return(NULL);
 	while (s[k])
 	{
@@ -62,9 +73,28 @@ char	**ft_strsplit(char const *s, char c)
 			k++;
 		else
 		{
-			s[k] = arr[k];
-			k++;
+			i = k;
+			while (s[k] != c && s[k])
+			{
+				arr[x] = ft_strsub(s, i, ft_wordlen((char *)s, c));
+				k++;
+			}
+			x++;
 		}
 	}
 	return (arr);
+}
+int		main(void)
+{
+	int		i;
+	char	**str;
+
+	i = 0;
+	str = ft_strsplit("Hello**World", '*');
+	while (i < ft_wordcount("Hello**World", '*'))
+	{
+		ft_putendl(str[i]);
+		i++;
+	}
+	return (0);
 }
